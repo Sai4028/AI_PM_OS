c30322df1a95ae13e9b2b8c5fa8c3552c10060f7
Sai4028 <malleli.saiwrk@gmail.com>
2026-06-01 19:56:14 +0530

    Update create_initiative.py

--- pages/create_initiative.py
+++ pages/create_initiative.py
@@ -1,1 +1,56 @@
+import streamlit as st
 
+from database.db import (
+    create_initiative
+)
+
+st.title("➕ Create Initiative")
+
+name = st.text_input(
+    "Initiative Name"
+)
+
+industry = st.text_input(
+    "Industry"
+)
+
+problem_statement = st.text_area(
+    "Problem Statement"
+)
+
+target_users = st.text_area(
+    "Target Users"
+)
+
+business_goal = st.text_area(
+    "Business Goal"
+)
+
+if st.button(
+    "Create Initiative"
+):
+
+    if not name:
+
+        st.error(
+            "Please enter Initiative Name"
+        )
+
+    else:
+
+        create_initiative(
+            name,
+            industry,
+            problem_statement,
+            target_users,
+            business_goal
+        )
+
+        st.success(
+            "Initiative Created Successfully!"
+        )
+
+        st.page_link(
+            "app.py",
+            label="⬅ Back to Dashboard"
+        )
